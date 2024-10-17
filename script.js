d9b0b584170c0551990e38ec3b1cd5cf747503a1
A Taher Basha <taherbashaengineer@gmail.com>
2024-10-17 20:57:55 +0530

    Added about the Execution Context and the Hoisting

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
-console.log("Hello This is JavaScript")
-console.log("Hello This is JavaScript2")
-console.log("This is javaScript3")
+// console.log("Hello This is JavaScript")
+// console.log("Hello This is JavaScript2")
+// console.log("This is javaScript3")
+
+
+// Hoisting in javaScript :
+/* Hoisting in js means we can use the variables before we can creates it*/  
+/*Lets understand it with an Example*/ 
+
+// console.log(x)
+// console.log(getName())
+
+// var x = 7;
+
+// function getName(){
+//     console.log("Hello JavaScript")
+// }
+
+
+// console.log(printName);
+
+// console.log(a);
+
+// var a = 3;
+
+// var printName = function (name) {
+//         console.log(name)
+//         var x = 20; 
+// }
+
+// Lexical Scoping
+
+// var a = 2;
+
+// var c = 2;
+
+
+
+// function b(){
+
+// var x = 2;
+
+// var c = 4
+
+// console.log(c)
+
+// }
+
+
+
+// console.log(a);
+
+// console.log(this.a);
+
+// console.log(this.c)
+
+// console.log(this.x)
+
+// console.log(window.a)
+
+// console.log(window.x)
+
+// console.log(b());
+
+a =10;
+
+console.log(a)
